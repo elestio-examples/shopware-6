726f101bc6b59aca3ec52b1cff9f95a00c991e9f
Amit Shukla <53058928+amitshuklabag@users.noreply.github.com>
2024-01-11 21:02:00 +0530

    Update preInstall.sh

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -1,10 +1,24 @@
 set env vars
 set -o allexport; source .env; set +o allexport;
 
-mkdir -p -m 750 ./data
-mkdir -p -m 750 ./db_volume
-mkdir -p -m 750 ./shop_volume
-
-chown -R 1000:1000 ./data
-chown -R 1000:1000 ./db_volume
-chown -R 1000:1000 ./shop_volume
+mkdir -p -m 750 ./db
+mkdir -p -m 750 ./state
+mkdir -p -m 750 ./plugins
+mkdir -p -m 750 ./files
+mkdir -p -m 750 ./jwt
+mkdir -p -m 750 ./theme
+mkdir -p -m 750 ./media
+mkdir -p -m 750 ./thumbnail
+mkdir -p -m 750 ./bundles
+mkdir -p -m 750 ./sitemap
+      
+chown -R 1000:1000 ./db
+chown -R 1000:1000 ./state
+chown -R 1000:1000 ./plugins
+chown -R 1000:1000 ./files
+chown -R 1000:1000 ./jwt
+chown -R 1000:1000 ./theme
+chown -R 1000:1000 ./media
+chown -R 1000:1000 ./thumbnail
+chown -R 1000:1000 ./bundles
+chown -R 1000:1000 ./sitemap
